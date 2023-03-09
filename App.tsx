7ec8f4d858aeb94a4e46cc7222424c33bea81441
shubham10101 <47697644+shubham10101@users.noreply.github.com>
2023-03-09 12:51:42 +0530

    1 file modified

--- App.tsx
+++ App.tsx
@@ -41,12 +41,27 @@ export default function App() {
           admin: false
       },   
   ]
+  const newUser ={   
+    id:3,
+    name: 'utkarsh',
+    age: 22,
+    admin: false
+}
   
   const reducer =(state,action )=>{
 
     switch (action.type){
       case "DeleteUser" : 
       return   state.filter((user)=> user.id !=action.id)
+      case "AddUser": 
+      return  [...state ,action.newUser]
+      case "UpdateUser" :
+      return  state.map((user)=>{
+          if(user.id == action.id){
+            return  {...user ,name :"ss"}
+          }
+         else return user
+        })
     }
   }
      
@@ -55,14 +70,23 @@ export default function App() {
 const handleDelete =(id)=>{
     dispatch({type :"DeleteUser" , id: id})
 }
+const handleAdd =(newUser)=>{
+    dispatch({type:"AddUser" ,newUser})
+}
+const handleUpdate= (id)=>{
+  dispatch({type:"UpdateUser" ,id})
+}
   return ( <div>
           {user.map((user)=>(
               <div id="user"  key={user.id}>
                     <h2>name :{user.name}</h2>
                     <p>age :{user.age}</p>
                     <button onClick={()=>handleDelete(user.id)}>delete user</button>
+                    <button onClick={()=>handleUpdate(user.id)}>update user</button>
+
                </div>
           ))}
+          <button onClick={()=>handleAdd(newUser)}>add user</button>
 
   </div>)
 }
